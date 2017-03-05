ebbd87d206ecb0b8e5c6e4661664f0a31f548e69
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-03-05 16:26:00 +0100

    capricorn: Use capricorn kernel config
    Change-Id: I6231a4a01ef70e43e69b0bdb2adc4e8928264295

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
+# Kernel
+TARGET_KERNEL_CONFIG := capricorn_defconfig
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
