8ebcc224a55d643c8198632dc6ab35e93324bbb2
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-03-05 20:26:49 +0100

    capricorn: Add fingerprint
    Change-Id: If3b2527585ec32caddc457e1293f86fd27b6f1af

--- lineage.mk
+++ lineage.mk
@@ -4,8 +4,15 @@ $(call inherit-product, device/xiaomi/capricorn/full_capricorn.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_capricorn
-PRODUCT_BRAND := xiaomi
-PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_DEVICE := capricorn
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Mi 5s
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="capricorn" \
+    PRODUCT_NAME="capricorn" \
+    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/7.4.27:user/release-keys" \
+    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T 7.4.27 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
