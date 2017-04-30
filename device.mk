adde2297b77e6a3fcb4f1ca5054423eef6497eae
LuK1337 <priv.luk@gmail.com>
2017-04-30 17:26:57 +0200

    capricorn: Build ConfigPanel

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
 
+# ConfigPanel
+PRODUCT_PACKAGES += \
+    ConfigPanel
+
 # Device init scripts
 PRODUCT_PACKAGES += \
     init.target.rc
