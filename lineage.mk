65a4527fb1b499ec35bf27e663528f7ddab2b2cb
LuK1337 <priv.luk@gmail.com>
2017-05-15 13:32:13 +0200

    capricorn: Update blobs from 7.0 release
    Change-Id: I72f3017362b0ae78892d1de2d201572e2d6eb55a

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="capricorn" \
     PRODUCT_NAME="capricorn" \
-    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys" \
-    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
+    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:7.0/NRD90M/7.5.15:user/release-keys" \
+    PRIVATE_BUILD_DESC="capricorn-user 7.0 NRD90M 7.5.15 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
