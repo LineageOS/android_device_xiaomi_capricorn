ec1eb87da1446342779d490c0c1b0cdc0a26c534
Bruno Martins <bgcngm@gmail.com>
2017-05-15 09:18:13 +0000

    capricorn: Update build fingerprint
     * From China Stable V8.2.4.0.MAGCNDL (MIUI8)
    
    Change-Id: I8957ed46f53d75c518ec39d9ef406352131b1699

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="capricorn" \
     PRODUCT_NAME="capricorn" \
-    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/7.4.27:user/release-keys" \
-    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T 7.4.27 release-keys"
+    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
