01bffb3a05b7061a6d17950ba625cfdb070c03ed
Luca Stefani <luca020400@lineageos.org>
2017-06-25 12:57:36 +0200

    capricorn: Move bdroid header to common
    Change-Id: I8dceec829bc5eecd4d16ac225785ced10b0844b9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,9 +24,6 @@ TARGET_OTA_ASSERT_DEVICE := capricorn
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-
 # Kernel
 TARGET_KERNEL_CONFIG := capricorn_defconfig
 
