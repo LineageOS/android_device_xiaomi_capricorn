d1e5c64f258064c6753a467cf469e33c05e4e4c3
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    capricorn: Inherit from Lineage vendor
    Change-Id: Ic22c1ee41ded6fcc389e99fcc75e02959ef0fc0f

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/xiaomi/capricorn/full_capricorn.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_capricorn
 PRODUCT_DEVICE := capricorn
