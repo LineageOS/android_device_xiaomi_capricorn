a651e1cdc52f6f897ef05e8f8b70e0b789a83eba
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    capricorn: Add basic HIDL manifest
     * This defines the interfaces for graphics and wifi
    
     * Placed out of common tree to be able to customize it
       per device
    
    Change-Id: I2c3aece8ad13613ec5b83496aa23d26ef238f45d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_OTA_ASSERT_DEVICE := capricorn
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := capricorn_defconfig
 
