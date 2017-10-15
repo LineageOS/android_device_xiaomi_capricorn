dffa18066463fbed524d335d615b1b480c13e26b
LuK1337 <priv.luk@gmail.com>
2017-10-15 17:05:40 +0200

    capricorn: Move audio configuration files to /vendor/etc
    Change-Id: I859beac0e6dbfe7dd660da3e339a0d069a1741e4

--- device.mk
+++ device.mk
@@ -24,8 +24,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/vendor/etc/mixer_paths_tasha.xml
 
 # ConfigPanel
 PRODUCT_PACKAGES += \
