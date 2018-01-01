3b73daf1ab8b887b8fede73e4648d98610b230ee
LuK1337 <priv.luk@gmail.com>
2018-01-01 23:10:58 +0100

    capricorn: Implement on boot TFA98xx calibration
    * TFA amplifier requires to play a calibration
      sound file on each boot otherwise it calibrates
      on the first played sound eg: unlock phone sound.
    
    Change-Id: Ice91c29d14e30ed688007cffff56375b178106c2

--- device.mk
+++ device.mk
@@ -53,5 +53,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# TFA calibration
+PRODUCT_PACKAGES += \
+    init.tfa.sh \
+    tinyplay
+
 # Inherit from msm8996-common
 $(call inherit-product, device/xiaomi/msm8996-common/msm8996.mk)
