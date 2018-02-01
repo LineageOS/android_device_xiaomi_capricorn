0fd58b1756673f66fd869d138e5477a674cf956c
Bruno Martins <bgcngm@gmail.com>
2018-02-01 22:04:45 +0100

    capricorn: Set matching build fingerprint for system, vendor and boot images
    Change-Id: I555c15eddc6dff694285ab1b23f6f9adfd64e535

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="capricorn" \
     PRODUCT_NAME="capricorn" \
-    BUILD_FINGERPRINT="Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys" \
     PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
