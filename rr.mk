41a626a2bb9ae7d3e711e6e680fa8c476a521d6d
Horyeong Park <necoritol@gmail.com>
2018-06-14 16:06:59 +0900

    Create rr.mk

---
+++ rr.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/xiaomi/capricorn/full_capricorn.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_capricorn
+PRODUCT_DEVICE := capricorn
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 5s
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="capricorn" \
+    PRODUCT_NAME="capricorn" \
+    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
