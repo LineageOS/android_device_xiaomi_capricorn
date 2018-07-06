b893595efe0c05a4715fbadaa8519c4acac48fa1
Bruno Martins <bgcngm@gmail.com>
2018-07-06 23:02:06 +0100

    capricorn: Update to the latest MIUI 9 Global Stable build fingerprint
    Change-Id: I23c6f1553b28eb59d3292379d5373790660c953e

--- lineage.mk
+++ lineage.mk
@@ -12,9 +12,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="capricorn" \
     PRODUCT_NAME="capricorn" \
-    PRIVATE_BUILD_DESC="capricorn-user 6.0.1 MXB48T V8.2.4.0.MAGCNDL release-keys"
+    PRIVATE_BUILD_DESC="capricorn-user 7.0 NRD90M V9.5.4.0.NAGMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:6.0.1/MXB48T/V8.2.4.0.MAGCNDL:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/capricorn/capricorn:7.0/NRD90M/V9.5.4.0.NAGMIFD:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
