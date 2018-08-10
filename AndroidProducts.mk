cc2ae2582326caaa2a9df214a087ed3a2fb8ac3e
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    capricorn: Consider lineage_capricorn product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_capricorn.mk
+    $(LOCAL_DIR)/lineage_capricorn.mk
