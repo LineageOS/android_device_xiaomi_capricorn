2e72c15465efb051827e7cad2584d11acb8f64c3
LuK1337 <priv.luk@gmail.com>
2018-08-16 15:09:27 +0200

    capricorn: Build fingerprint 2.1 service
    * Turns out that our fingerprint HAL is actually
      2.1 compatible and modifying module version from
      0x200 to 0x201 makes it work with 2.1 service out
      of box.
    
    Change-Id: I46578b191b76cfcce0fc85b17e04774b98a30629
    (cherry picked from commit ded7db2f60ee5431703c3fd798e9db70a7813655)

--- device.mk
+++ device.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # FlipFlap
 PRODUCT_PACKAGES += \
