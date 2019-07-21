483677da54bd80f0b2785f600ec2dc5d8c36b7e6
LuK1337 <priv.luk@gmail.com>
2019-07-22 00:29:21 +0200

    capricorn: Update fingerprint HAL from 8.0
    * HAL is patched to register itself as 2.1
      instead of 3.0 using these commands:
        sed -i 's/_ZN7android8hardware10biometrics11fingerprint4V3_0/_ZN7android8hardware10biometrics11fingerprint4V2_1/g' vendor/bin/qfp-daemon
        sed -i 's/android.hardware.biometrics.fingerprint@3.0.so/android.hardware.biometrics.fingerprint@2.1.so/' vendor/bin/qfp-daemon
        sed -i 's/_ZN7android8hardware10biometrics11fingerprint4V3_0/_ZN7android8hardware10biometrics11fingerprint4V2_1/g' vendor/lib64/libqfp-service.so
        sed -i 's/android.hardware.biometrics.fingerprint@3.0.so/android.hardware.biometrics.fingerprint@2.1.so/' vendor/lib64/libqfp-service.so
    
    Change-Id: I44c48d617cfdd10e973386bab7ba4a0d89eed781

--- device.mk
+++ device.mk
@@ -42,10 +42,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
-
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
