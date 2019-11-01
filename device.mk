bb9d2ae96e15f7e1af7897b30d3e4798b7a6660f
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    capricorn: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device.mk
+++ device.mk
@@ -73,5 +73,8 @@ PRODUCT_PACKAGES += \
     init.tfa.sh \
     tinyplay
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
 # Inherit from msm8996-common
 $(call inherit-product, device/xiaomi/msm8996-common/msm8996.mk)
